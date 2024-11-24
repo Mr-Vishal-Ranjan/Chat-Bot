9213318c2e2407a308a4f5584da3564fa8a8f52f
Vishal Ranjan <>
2024-11-24 16:20:42 +0530

    Error handling and font fix

--- src/App.jsx
+++ src/App.jsx
@@ -42,100 +42,121 @@ function App() {
   }
 
   return (
-    <div className="full h-min-screen">
-      <div className="flex row wrap">
-        <SettingsSection
-          aiPersonality={aiPersonality}
-          setPersonality={setPersonality}
-          apiKey={apiKey}
-          setApiKey={setApiKey}
-          resetConversation={resetConversation}
-          engineId={engineId}
-          setEngineId={setEngineId}
-        />
-        <section
-          id="content"
-          className="full md:half lg:screen-v-scroll flex row wrap relative"
-        >
-          <div className="full md:py px">
-            <center>
-              <span className="large title white">Chatbot</span>
-            </center>
-            <br />
-            <br />
+    <>
+      <div className="full h-min-screen">
+        <div className="flex row wrap">
+          <SettingsSection
+            aiPersonality={aiPersonality}
+            setPersonality={setPersonality}
+            apiKey={apiKey}
+            setApiKey={setApiKey}
+            resetConversation={resetConversation}
+            engineId={engineId}
+            setEngineId={setEngineId}
+          />
+          <section
+            id="content"
+            className="full md:half lg:screen-v-scroll flex row wrap relative"
+          >
+            <div className="full md:py px">
+              <center>
+                <span className="large title white">Chatbot</span>
+              </center>
+              <br />
+              <br />
 
-            <MainContent
-              showConversationDebug={showConversationDebug}
-              conversationHistory={conversationHistory}
-              setShowConversationDebug={setShowConversationDebug}
-              loading={isLoading}
-              apiKey={apiKey}
-              aiName={aiPersonality}
-            />
-            {conversationHistory.map((item, index) => {
-              if (item.sender === Sender.HUMAN) {
-                return (
-                  <span key={index} className={"message-text"}>
-                    <span>You:</span> <span className="blue">{item?.text}</span>
-                  </span>
-                );
-              } else {
-                return (
-                  <>
-                    <br />
-                    <span key={index} className={"message-text"}>
-                      <span>{aiPersonality}:</span>{" "}
-                      <span
-                        className="green"
-                        dangerouslySetInnerHTML={{ __html: item?.text }}
-                      ></span>
-                    </span>
-                  </>
-                );
-              }
-            })}
-            {isLoading ? (
-              <>
-                <span className={"message-text"}>
-                  <span>You:</span> <span className="blue">{question}</span>
-                </span>
-                <div style={{ textAlign: "center" }}>
-                  <Loader />
-                  <br />
-                </div>
-              </>
-            ) : (
-              <span>
-                <br />
-                <br />
-              </span>
-            )}
-
-            <form onSubmit={poseQuestion}>
-              <input
-                type="text"
-                value={question}
-                placeholder={"Ask a question..."}
-                spellCheck={false}
-                onChange={(e) => setQuestion(e.target.value)}
+              <MainContent
+                showConversationDebug={showConversationDebug}
+                conversationHistory={conversationHistory}
+                setShowConversationDebug={setShowConversationDebug}
+                loading={isLoading}
+                apiKey={apiKey}
+                aiName={aiPersonality}
               />
-              {errorMessage && (
-                <span className={"fuschia"}>{errorMessage}</span>
+              {conversationHistory.map((item, index) => {
+                if (item.sender === Sender.HUMAN) {
+                  return (
+                    <span key={index} className={"message-text"}>
+                      <span>You:</span>{" "}
+                      <span className="blue">{item?.text}</span>
+                    </span>
+                  );
+                } else {
+                  return (
+                    <>
+                      <br />
+                      <span key={index} className={"message-text"}>
+                        <span>{aiPersonality}:</span>{" "}
+                        <span
+                          className="green"
+                          dangerouslySetInnerHTML={{ __html: item?.text }}
+                        ></span>
+                      </span>
+                    </>
+                  );
+                }
+              })}
+              {isLoading ? (
+                <>
+                  <span className={"message-text"}>
+                    <span>You:</span> <span className="blue">{question}</span>
+                  </span>
+                  <div style={{ textAlign: "center" }}>
+                    <Loader />
+                    <br />
+                  </div>
+                </>
+              ) : (
+                <span>
+                  <br />
+                  <br />
+                </span>
               )}
-              <button
-                type="submit"
-                className="button"
-                disabled={isLoading}
-              ></button>
-            </form>
-          </div>
-        </section>
-        <AsideSection
-          showConversationDebug={showConversationDebug}
-          setShowConversationDebug={setShowConversationDebug}
-        />
+
+              <form onSubmit={poseQuestion}>
+                <input
+                  type="text"
+                  value={question}
+                  placeholder={"Ask a question..."}
+                  spellCheck={false}
+                  onChange={(e) => setQuestion(e.target.value)}
+                  style={{ border: "1px solid #033a" }}
+                />
+                {errorMessage && (
+                  <span className={"fuschia"}>{errorMessage}</span>
+                )}
+                <button
+                  type="submit"
+                  className="button"
+                  disabled={isLoading}
+                ></button>
+              </form>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <div className="submit" onClick={poseQuestion}>
+                  Submit
+                </div>
+              </div>
+            </div>
+          </section>
+          <AsideSection
+            showConversationDebug={showConversationDebug}
+            setShowConversationDebug={setShowConversationDebug}
+          />
+        </div>
       </div>
-    </div>
+      <style jsx>{`
+        .submit {
+          border: 1px solid #19b5ff;
+          padding: 8px 16px;
+          border-radius: 40px;
+          &:hover {
+            cursor: pointer;
+            background: #19b5ff;
+            color: black;
+          }
+        }
+      `}</style>
+    </>
   );
 }
 
--- src/components/useAi.jsx
+++ src/components/useAi.jsx
@@ -21,40 +21,45 @@ export const useAI = (initialPersonality) => {
 
   const getResponse = async () => {
     setIsLoading(true);
-    const prompt =
-      conversation + `\n${Sender.HUMAN}: ` + question + `\nHelpful AI: `;
+    try {
+      const prompt =
+        conversation + `\n${Sender.HUMAN}: ` + question + `\nHelpful AI: `;
 
-    const response = await fetch(
-      "https://api-inference.huggingface.co/models/Qwen/Qwen2.5-72B-Instruct/v1/chat/completions",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + "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",
-        },
-        body: JSON.stringify({
-          model: "Qwen/Qwen2.5-72B-Instruct",
-          messages: [{ role: "user", content: prompt }],
-          temperature: 0.5,
-          max_tokens: 2048,
-          top_p: 0.7,
-        }),
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/Qwen/Qwen2.5-72B-Instruct/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + "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",
+          },
+          body: JSON.stringify({
+            model: "Qwen/Qwen2.5-72B-Instruct",
+            messages: [{ role: "user", content: prompt }],
+            temperature: 0.5,
+            max_tokens: 2048,
+            top_p: 0.7,
+          }),
+        }
+      );
+      if (response?.status == 200) {
+        const data = await response.json();
+        console.log(data);
+
+        const text = data.choices[0]?.message?.content;
+
+        if (text && text.length > 0) {
+          setConversation(prompt + text);
+          setConversationHistory([
+            ...conversationHistory,
+            { sender: Sender.HUMAN, text: question },
+            { sender: aiPersonality, text: text },
+          ]);
+          setQuestion("");
+        }
       }
-    );
-
-    const data = await response.json();
-    console.log(data);
-
-    const text = data.choices[0]?.message?.content;
-
-    if (text && text.length > 0) {
-      setConversation(prompt + text);
-      setConversationHistory([
-        ...conversationHistory,
-        { sender: Sender.HUMAN, text: question },
-        { sender: aiPersonality, text: text },
-      ]);
-      setQuestion("");
+    } catch (error) {
+      console.error(error);
     }
 
     setIsLoading(false);
